42d7ed75f46e90107085ac3e599e12a573021444
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-21 20:31:51 +0300

    add antipattern

--- venv/lesson_01/task_01.py
+++ venv/lesson_01/task_01.py
@@ -8,4 +8,15 @@ print(id(a))
 a = 3.14 / 3
 print(id(a))
 
-print(name, age, a, 101, 'text', sep=' * ')
+print(name, age, a, 101, 'text', sep=' * ', end='#')
+print('any text')
+
+res = input("Print your text: ")
+print('Ты написал:', res)
+
+age = int(input('Сколько тебе лет? '))
+print(age)
+
+age = float(input('Ваш возраст: '))
+how_old = age - 18
+print(how_old, "лет назад ты стал совершеннолетним")
