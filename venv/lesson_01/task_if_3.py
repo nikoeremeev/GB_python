5e791ab2f5921785fa3645bccd25f81a0785c063
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-21 20:47:12 +0300

    add task_if_3.py file

---
+++ venv/lesson_01/task_if_3.py
@@ -0,0 +1,8 @@
+pwd = 'text'
+res = input('Input password ')
+if res == pwd:
+    print('Доступ разрешен')
+    print('Но будьте осторожны')
+else:
+    print('Доступ запрещен')
+print('Работа завершена')
