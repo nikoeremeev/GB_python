983de7340af81529b4e0f5d629a78ce85c9dfeb1
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-25 06:50:00 +0300

    add task_03.py

--- venv/seminar_01/task_03.py
+++ venv/seminar_01/task_03.py
@@ -2,4 +2,11 @@
 Выведите в консоль таблицу умножения от 2х2 до 9х10 как на
 школьной тетрадке.
 '''
-
+first_num = 2
+while first_num < 10:
+    second_num = 2
+    while second_num <= 10:
+        print(f"{first_num} * {second_num} = {first_num * second_num}")
+        second_num += 1
+    print()
+    first_num += 1
