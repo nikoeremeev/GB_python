7a8c270ba727bdc12b71a555797380137aa6440e
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-26 20:36:56 +0300

    done task_07.py

--- venv/seminar_02/task_07.py
+++ venv/seminar_02/task_07.py
@@ -4,3 +4,19 @@
 строковое представление. Функцию hex
 используйте для проверки своего результата.
 """
+
+
+def convert_to(number, base=16):
+    digits = '0123456789abcdef'
+    if base > len(digits):
+        return None
+    result = ''
+    while number > 0:
+        result = digits[number % base] + result
+        number //= base
+    return result
+
+
+num = int(input("Введите число для перевода в hex-формат: "))
+print(f"В шестнадцатеричном виде число {num} равно {convert_to(num)}.")
+print(hex(num))
