de5f2044eda037079bfb0a4d71a92a458eb5b005
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-26 15:03:02 +0300

    done task_05.py

--- venv/seminar_02/task_05.py
+++ venv/seminar_02/task_05.py
@@ -5,3 +5,26 @@
 ✔ Используйте комплексные числа
 для извлечения квадратного корня.
 """
+
+from math import *
+from random import *
+import cmath
+
+a = int(input("Введите число А: "))
+b = int(input("Введите число B: "))
+c = int(input("Введите число C: "))
+
+D = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % D)
+
+if D > 0:
+    x1 = (-b + sqrt(D)) / (2 * a)
+    x2 = (-b - sqrt(D)) / (2 * a)
+    print(f"x1 = {x1}; x2 = {x2}")
+elif D == 0:
+    x = -b / (2 * a)
+    print(f"x = {x1}")
+else:
+    x1 = (-b + cmath.sqrt(D)) / (2 * a)
+    x2 = (-b - cmath.sqrt(D)) / (2 * a)
+    print(f"x1 = {x1}; x2 = {x2}")
