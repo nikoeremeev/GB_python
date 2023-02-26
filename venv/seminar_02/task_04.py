11f37c0d8010dd1a1a45a05eb5bb5157a6c75dc4
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-02-26 14:53:45 +0300

    done task_04.py

--- venv/seminar_02/task_04.py
+++ venv/seminar_02/task_04.py
@@ -5,3 +5,22 @@
 ✔ Точность вычислений должна составлять
 не менее 42 знаков после запятой.
 """
+
+import math
+
+
+def area_of_the_circle(diameter):
+    return round(1 / 4 * math.pi * diameter ** 2, 42)
+
+
+def circumference_length(diameter):
+    return round(math.pi * diameter, 42)
+
+
+diameter = int(input(
+    "Введите диаметр для вычисления площади круга и длины окружности: "))
+while diameter > 1000:
+    diameter = int(input(
+        "Введите диаметр меньше 1000!: "))
+print(f"Площадь круга равна: {area_of_the_circle(diameter)}.")
+print(f"Длина окружности равна: {circumference_length(diameter)}.")
