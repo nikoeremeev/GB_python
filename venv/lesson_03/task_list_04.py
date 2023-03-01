adb1ea7f2fb6b0cbb00feabcba3874b559c9aeac
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-01 21:52:54 +0300

    add task_list_04.py

--- venv/lesson_03/task_list_04.py
+++ venv/lesson_03/task_list_04.py
@@ -2,7 +2,7 @@ a = 42
 b = 'Hello world!'
 c = [1, 3, 5, 7]
 my_list = [None]
-my_list.extend(a) # TypeError: 'int' object is not iterable
+#my_list.extend(a) # TypeError: 'int' object is not iterable
 print(my_list)
 my_list.extend(b)
 print(my_list)
