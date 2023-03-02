6a2bbd22f236cbc151bcbcdb34496118bc3766a4
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-02 21:41:21 +0300

    add task_list_17.py

---
+++ venv/lesson_03/task_list_17.py
@@ -0,0 +1,13 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+new_m = matrix.copy()
+print(matrix, new_m, sep='\n')
+matrix[0][1] = 555
+print(matrix, new_m, sep='\n')
+
+import copy
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+new_m = copy.deepcopy(matrix)
+print(matrix, new_m, sep='\n')
+matrix[0][1] = 555
+print(matrix, new_m, sep='\n')
