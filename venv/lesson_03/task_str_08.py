a80dfb92baba9ad196a4a106fc7458b963b1b6df
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-03 20:01:19 +0300

    change task_str_09.py

--- venv/lesson_03/task_str_08.py
+++ venv/lesson_03/task_str_08.py
@@ -3,3 +3,5 @@ urls = link.split('/')
 print(urls)
 a, b, c = input('Введите 3 числа через пробел: ').split()
 print(c, b, a)
+
+a, b, c, *_ = input('Введите не менее трёх чисел через пробел: ').split()
