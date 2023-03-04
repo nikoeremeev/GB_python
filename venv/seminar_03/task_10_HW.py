017d79041f851278f555b7fdfd41ef62d3a7363f
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-04 21:21:41 +0300

    add task_10_HW.py

--- venv/seminar_03/task_10_HW.py
+++ venv/seminar_03/task_10_HW.py
@@ -5,3 +5,19 @@
 из википедии или из документации к языку.
 
 """
+
+text = input("Введите текст: ").lower() \
+    .replace(',', '').replace('.', '').replace('!', '').replace('?', '').split(' ')
+my_dict = dict()
+for item in text:
+    if item not in my_dict.keys():
+        my_dict[item] = 1
+    else:
+        my_dict[item] += 1
+my_dict = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+l = 10
+if len(my_dict) <= 10:
+    l = len(my_dict)
+print(my_dict)
+for i in range(l):
+    print(list(my_dict)[i], end=' ')
