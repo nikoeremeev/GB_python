50de8a3a7913971519125f022d4812df79fc5ad5
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-04 21:05:03 +0300

    add task_09_HW.py

--- venv/seminar_03/task_09_HW.py
+++ venv/seminar_03/task_09_HW.py
@@ -3,3 +3,14 @@
 с дублирующимися элементами. В результирующем списке
 не должно быть дубликатов.
 """
+
+init_lst = [5, 7, 8, 6, 9, 10, 2, 5, 5, 4, 6, 7, 8]
+dub_lst = []
+res = []
+for item in init_lst:
+    if init_lst.count(item) > 1:
+        dub_lst.append(item)
+    else:
+        res.append(item)
+print(f"Список дублирующихся элементов: {dub_lst}")
+print(f"Результирующий список: {res}")
