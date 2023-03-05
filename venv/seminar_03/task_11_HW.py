47c4ab3756c4a353dd633d36748b0660739a006f
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-05 17:40:22 +0300

    add task_11_HW.py

--- venv/seminar_03/task_11_HW.py
+++ venv/seminar_03/task_11_HW.py
@@ -5,3 +5,23 @@
 грузоподъёмность. Достаточно вернуть один допустимый вариант.
 ✔ *Верните все возможные варианты комплектации рюкзака.
 """
+
+
+def backpack(shop: dict[str:int], size: int) -> list[list[str]]:
+    pcs, weight = zip(*sorted(shop.items(), key=lambda x: x[1], reverse=True))
+    result, temp, temp_w = [], [], 0
+    for index, w in enumerate(weight, 0):
+        temp_w += w
+        temp.append((pcs[index]))
+        for index_n, wn in enumerate(weight[index:], index):
+            if wn + temp_w <= size:
+                temp_w += wn
+                temp.append(pcs[index_n])
+        result.append(temp)
+        temp, temp_w = [], 0
+    return result
+
+
+shop = {"носки": 1, "штаны": 3, "футболка": 2, "футболка_2": 2, "куртка": 5, "кепка": 1}
+backpack_size = 7
+[print(i) for i in backpack(shop, backpack_size)]
