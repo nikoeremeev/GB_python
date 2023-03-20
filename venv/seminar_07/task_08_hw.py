31ae86b6f13f60b9ef3f20b743b4e235c9b22c16
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-20 21:15:29 +0300

    add tasks

--- venv/seminar_07/task_08_hw.py
+++ venv/seminar_07/task_08_hw.py
@@ -12,7 +12,24 @@
     желаемое конечное имя, если оно передано. Далее счётчик файлов и расширение.
 """
 
+from pathlib import Path
 
-def group_rename_files(name_for_files: str, amount_digit: int, old_file_extension: str, new_file_extension: str,
-                       range_of_letters: tuple) -> None:
+__all__ = [
+    'group_rename_files',
+]
 
+
+def group_rename_files(directory: Path, name_for_files: str, amount_digit: int, old_file_extension: str,
+                       new_file_extension: str, range_of_letters: list[int]) -> None:
+    for i, file in enumerate(directory.glob(f'*.{old_file_extension}'), start=1):
+        file_name_, _ = file.name.split('.')
+        start = min(range_of_letters[0], len(file_name_))
+        stop = min(range_of_letters[1], len(file_name_))
+        file_name = file_name_[start:stop]
+        new_file_name = f'{file_name}{name_for_files}{i:0{amount_digit}}.{new_file_extension}'
+
+        file.rename(file.parent / new_file_name)
+
+    if __name__ == '__main__':
+        group_rename_files(Path('files'), name_for_files='_test_', amount_digit=3, old_file_extension='bin',
+                           new_file_extension='txt', range_of_letters=[2, 6])
