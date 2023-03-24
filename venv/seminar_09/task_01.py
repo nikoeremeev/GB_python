c2f7a2765f3358f07fa6686bcfbe6f1af70bb5dc
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-03-24 07:47:01 +0300

    done task_01

--- venv/seminar_09/task_01.py
+++ venv/seminar_09/task_01.py
@@ -5,3 +5,25 @@
 Функция возвращает функцию, которая через консоль просит угадать загаданное число за указанное число попыток.
 """
 
+from random import randint
+
+
+def guess_the_number(number: int, count: int):
+    def wrapper():
+        for i in range(1, count + 1):
+            print(f"Попытка номер {i} из {count}.")
+            user_num = int(input("Введите число от 1 до 100: \n >>> "))
+            if user_num == number:
+                print("Угадал!!!")
+                break
+            elif user_num < number:
+                print("Ваше число меньше")
+            else:
+                print("Ваше число больше")
+
+    return wrapper
+
+
+if __name__ == '__main__':
+    temp = guess_the_number(randint(1, 100), randint(1, 10))
+    temp()
