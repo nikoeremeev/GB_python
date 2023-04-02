fcd4dc737aeb8d50db21200ef46f8254258c17b3
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-04-02 17:05:29 +0300

    add tasks from lesson_11

---
+++ venv/lesson_11/task_print_01.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, name: str):
+        """Added the name parameter."""
+        self.name = name
+
+    def simple_method(self):
+        """Example of a simple method."""
+        self.name.capitalize()
+
+
+user = User('Спенглер')
+print(user)
